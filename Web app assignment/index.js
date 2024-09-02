3048d8ff5c36dd2f8ef2be5d96f24520165892b1
oherss <48643884+oherss@users.noreply.github.com>
2024-09-02 11:44:24 +0200

    Lots of website crap

--- Web app assignment/index.js
+++ Web app assignment/index.js
@@ -1,18 +1,72 @@
 console.log("JS is loaded");
 let pages; 
+let textElements = [];
+let AboutPepper;
+let sources = [];
 let menuitems;
 let totalPages;
 let currentPage = 0;
 
+function preload(){
+    AboutPepper = loadStrings("./Assets/AboutPepper.txt")
+    sources = loadStrings("./Assets/Sources.txt")
+}
+
 function setup(){
     pages = selectAll(".Page");
+    let TempText = selectAll("p")
     menuitems = selectAll(".menuitem");
+    
     totalPages = pages.length-1;
     switchPage(currentPage);
+
+    for (let i = 0; i < sources.length; i++) {
+        sources[i] = JSON.parse(sources[i])
+    }
+    for (let i = 0; i < AboutPepper.length; i++) {
+        AboutPepper[i] = JSON.parse(AboutPepper[i])
+    }
 
     for(m of menuitems){
         let pnum = m.elt.id.slice(-1) - 1;
         m.mouseClicked(()=>{switchPage(pnum);});
+    }
+
+    for(p of TempText){
+        if(p.id =! null){
+            textElements.push(p)
+        }
+    }
+
+    for(p of textElements){
+        console.log("Total loaded amount of text lines: " + AboutPepper.length)
+        console.log("P id is: " + p.elt.id)
+        for(t of AboutPepper){
+            console.log("text id is: "+ t.id)
+            if(t.id == p.elt.id)
+            {
+                console.log("Found matching text for element: " + p.elt.id)
+                p.elt.innerHTML = t.text
+            }
+            if(p.elt.id == t.pageid+"Contents"){
+                let TempText =""
+                for(i of AboutPepper){
+                    TempText += ("- " + i.name +"<br>")
+                }
+                p.elt.innerHTML = TempText
+            }
+            if(p.elt.id == t.pageid+"Sources"){
+                let TempText =""
+                for(s of sources){
+                    if(s.pageid == t.pageid){
+                        TempText += "  -Segment: " + s.name + "<br>"
+                        TempText += "   Source: <a href='" + s.source + "'>" + s.text + "</a> <br>"
+                    }
+                }
+                p.elt.innerHTML = TempText
+            }
+        }
+        
     }
 }
 
