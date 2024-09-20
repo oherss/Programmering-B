d4ae3843be3a7eb26fc057dee0be839be1b77537
oherss <48643884+oherss@users.noreply.github.com>
2024-09-20 12:34:30 +0200

    Searching

--- API Templates collection/index.js
+++ API Templates collection/index.js
@@ -11,6 +11,12 @@ function setup(){
   glass = select("#glass")
   instruction = select("#Instructions")
   img = select("#DrinkImg")
+  DrinkIDText = select("#DrinkIDT")
+  DrinkIDInput = select("#DrinkIDInp")
+  DrinkIDButton = select("#DrinkIDBut")
+  DrinkSearchButton = select("#DrinkSearchBut")
+  DrinkNameSearchButton =select("#SearchDrinkNameButton")
+  DrinkNameSearchInput = select("#SearchDrinkNameInput")
     setupMenuStructure()
 }
 
@@ -40,6 +46,23 @@ function setupMenuStructure(){
 }
 
 function pageTwo(){
+DrinkIDButton.mousePressed(function(){
+  fetch(('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + DrinkIDInput.elt.value.toString()))
+  .then( function(response){
+    console.log(response)
+    console.log(DrinkIDInput)
+    return response.json()
+  })
+  .then( function(json){
+    console.log(json)
+    LoadDrink(json)
+    
+  })
+})
+DrinkSearchButton.mousePressed(function(e){
+  
+  shiftPage(5)
+})
 
   fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
   .then( function(response){
@@ -47,12 +70,62 @@ function pageTwo(){
     return response.json()
   })
     .then( function(json){
-      console.log(json)
-      data = json.drinks
+      LoadDrink(json)
+    })
+}
+
+function pageThree(){
+  fetch("./Assets/mydata.json")
+  .then(function(response){
+    console.log(response)
+    return response.json()
+  
+  })
+  .then( function(data){
+    console.log(data)
+            //p5 funktion der laver en ny div
+            let newDiv = createElement('div')
+            //så laver vi en overskrift
+            let newHeader = createElement('h1', data.Name)
+            //så laver vi et p-element
+            let newP = createElement('p', data.Description)
+            //nu laver vi en underoverskrift
+            let hairHeader = createElement('h3', 'Tidligere hårfarver')
+            //nu skal jeg løbe et array igennem fra json
+            let hairList = createElement('ul')
+            for( color of data.formerHairColors ){
+                //console.log(color)
+                let listItem = createElement('li', color)
+                hairList.child(listItem)
+            } 
+            //til sidst lægger vi de nye elementer ind i den div vi oprettede
+            newDiv.child(newHeader)
+            newDiv.child(newP)
+            newDiv.child(hairHeader)
+            newDiv.child(hairList)
+            //newDiv.child(createElement("img", data.image ))
+
+            //tag fat i html elementet med id = localData
+            //tøm det
+            select('#localData').html('')
+            //og sæt data ind i det 
+            select('#localData').child(newDiv)
+  })
+
+}
+
+function pageFour(){
+}
+
+function LoadDrink(json){
+  console.log(json) //Logging the raw data recieved
+      data = json.drinks //
       console.log(data)
       Drinkname.elt.innerHTML = data[0].strDrink
       Subtitle.elt.innerHTML = data[0].strAlcoholic + " " + data[0].strCategory
       img.elt.src = data[0].strDrinkThumb
+      DrinkIDText.elt.innerHTML = "Drink ID: " + data[0].idDrink
+
   
       let Ingredients = []
   
@@ -91,29 +164,38 @@ function pageTwo(){
     instruction.elt.innerHTML = data[0].strInstructions
     console.log(Ingredients)
       //brug data resultaterne...
+}
+function pageFive(){
+  console.log("page 5")
+  DrinkNameSearchButton.mousePressed(function(){
+    fetch(('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + DrinkNameSearchInput.elt.value.toString()))
+      .then( function(response){
+      console.log(response)
+      console.log(DrinkIDInput)
+      return response.json()
     })
-}
-
-function pageThree(){
-  fetch("./Assets/SampleData.json")
-  .then(function(response){
-    console.log(response)
-    return response.json()
-  
+    .then( function(json){
+      const Drinks = json.drinks
+      console.log(Drinks)
+      //select('#localData').html('')
+      for(drink of Drinks){
+        console.log(drink)
+        let newDiv = createElement('div')
+        newDiv.addClass("ListItem")
+        let newImg = createImg(drink.strDrinkThumb)
+        let newHeader = createElement('p', drink.strDrink)
+        newHeader.addClass("ListItem")
+        let newP = createElement('p', drink.idDrink)
+        newP.style("font-size", "1rem")
+        newImg.style("width","5rem")
+        newDiv.child(newImg)
+        newDiv.child(newHeader)
+        newDiv.child(newP)
+        select('#DrinkList').child(newDiv)
+      }
+      
+    })
   })
-  .then( function(json){
-    let newDiv = createElement("div")
-    let newHeader = createElement("h1", json.name)
-    let newP =createElement("p", json.job)
-    newDiv.child(newHeader)
-    newDiv.child(newP)
-    select("#localData").html("")
-    select("#localData").child(newDiv)
-  })
-
-}
-
-function pageFour(){
 }
 
 function shiftPage(num){
@@ -139,6 +221,9 @@ function shiftPage(num){
     }
     if(currentPage == 3) {
         pageThree()
+    }
+    if(currentPage == 5) {
+        pageFive()
     }
 }
 
